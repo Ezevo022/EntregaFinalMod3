a89c0b5a58072de3b6a801da61b5f7c1e9b0ae35
Ezevo022 <eze.22.xxi@gmail.com>
2025-06-10 21:24:20 -0300

    Se agrega carrito y se va ajustando a la barra de navegacion

--- src/components/CardProduct/index.jsx
+++ src/components/CardProduct/index.jsx
@@ -4,15 +4,18 @@ import { config } from "../../config"
 import { 
     Card,
     CardHeader,
-    CardMedia,
     CardContent,
     Typography,
     Button,
     Box,
 } from "@mui/material"
+import { manejarCarrito } from "../../store/cart"
+import { Image } from "antd"
 
 export const CardProduct = ({ id }) => {
     const [product, setProduct] = useState({})
+
+    const { agregar } = manejarCarrito();
 
     const fetch = async () => {
         const url = `${config.VITE_API_URL}products/${id}`
@@ -25,16 +28,29 @@ export const CardProduct = ({ id }) => {
         fetch()
     }, [])
 
+    const handleAddProducto = () => {
+        agregar(product);
+    };
+
   return (
     <Card sx={{ width: "350px", margin: "20px" }}>
         <CardHeader title={product.title} subheader={product.category} />
-        <CardMedia component={"img"} image={product.image} height="200" />
-        <CardContent>
-            <Typography variant="body1">{product.description}</Typography>
-            <Box sx={{ display: "flex", justifyContent: "space-evenly", mt: 2 }}>
-                <Button variant="contained">Comprar</Button>
-                <Button variant="outlined">Ver</Button>
-            </Box>
+        <CardContent sx={{ padding: 0 }}>
+        <Image
+          src={product.image}
+          height={200}
+          width={"100%"}
+          style={{ objectFit: "cover", margin: 0 }}
+        />
+        <Box sx={{ padding: 2 }}>
+          <Typography variant="body1">{product.description}</Typography>
+          <Box sx={{ display: "flex", justifyContent: "space-evenly", mt: 2 }}>
+            <Button variant="contained" onClick={handleAddProducto}>
+              Comprar
+            </Button>
+            <Button variant="outlined">Ver</Button>
+          </Box>
+        </Box>
         </CardContent>
     </Card>
 )
